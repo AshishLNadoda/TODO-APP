cbaf5d6cac4a00c85655fb21892a4e48baa83e5d
AshishLNadoda <ashishnadoda007@gmail.com>
2023-06-12 19:19:49 +0530

    upload todo app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
 import Footer from './components/weeklytest/footer/Footer'
 import Nav from './components/weeklytest/nav/Nav';
+import Bpp from './components/todo/Todo'
 
 function App() {
   
   return (
     <div className="App">
-      <Nav/>
-      <Footer/>
+     <Bpp/>
      
 
      
